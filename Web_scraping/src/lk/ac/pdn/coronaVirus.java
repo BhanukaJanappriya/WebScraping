4e8158802b0f8970c7b377fda7fe6e980f2dd89d
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-04-27 17:31:35 +0530

    Create countryData class and add getters setters

--- Web_scraping/src/lk/ac/pdn/coronaVirus.java
+++ Web_scraping/src/lk/ac/pdn/coronaVirus.java
@@ -1,9 +1,13 @@
 package lk.ac.pdn;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class coronaVirus {
 
@@ -14,6 +18,10 @@ public class coronaVirus {
 		CovidData globalData = scrapeGlobalData(url);
 		System.out.println("Global Covid-19 Data: ");
 		System.out.println(globalData.toString());
+		
+		List<CountryData> countryData = scrapeCountryData(url);
+		System.out.println("\nTop 20 Countries by cases: ");
+		countryData.stream().limit(20).forEach(System.out::println);
 		
 		
 	}
@@ -29,6 +37,29 @@ public class coronaVirus {
 		data.setClosedCases(doc.select("div.number-table-main").get(1).text());
 		
 		return data;
+	}
+	
+	private static List<CountryData>  scrapeCountryData(String url) throws IOException {
+		Document doc = Jsoup.connect(url).get();
+		
+		List<CountryData> countries = new ArrayList<>();
+		Elements rows = doc.select("table#main_table_countries_today tbody tr");
+		
+		for (Element row : rows) {
+			Elements cols = row.select("td");
+			if(cols.size()>1) {
+				CountryData country = new CountryData();
+				country.setName(cols.get(1).text());
+				country.setTotalCases(cols.get(2).text());
+				country.setNewCases(cols.get(3).text());
+				country.setTotalDeaths(cols.get(4).text());
+				country.setNewDeaths(cols.get(5).text());
+				country.setTotalRecovered(cols.get(6).text());
+				
+				countries.add(country);
+			}
+		}
+		return countries;
 	}
 	//structured our downloaded data
 	static  class CovidData{
@@ -78,5 +109,60 @@ public class coronaVirus {
 		}
 		
 	}
+	
+	static class CountryData{
+		private String name;
+		private String totalCases;
+		private String newCases;
+		private String totalDeaths;
+		private String newDeaths;
+		private String totalRecovered;
+		
+		//getter and setters
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getTotalCases() {
+			return totalCases;
+		}
+		public void setTotalCases(String totalCases) {
+			this.totalCases = totalCases;
+		}
+		public String getNewCases() {
+			return newCases;
+		}
+		public void setNewCases(String newCases) {
+			this.newCases = newCases;
+		}
+		public String getTotalDeaths() {
+			return totalDeaths;
+		}
+		public void setTotalDeaths(String totalDeaths) {
+			this.totalDeaths = totalDeaths;
+		}
+		public String getNewDeaths() {
+			return newDeaths;
+		}
+		public void setNewDeaths(String newDeaths) {
+			this.newDeaths = newDeaths;
+		}
+		public String getTotalRecovered() {
+			return totalRecovered;
+		}
+		public void setTotalRecovered(String totalRecovered) {
+			this.totalRecovered = totalRecovered;
+		}
+		
+		public String toString() {
+			return String.format("%-20s | Cases: %10s | New: %s | Deaths: %-10s | Recovered: %-10s",name,totalCases,newCases,totalDeaths,totalRecovered);
+		}
+		
+		
+	}
+	
+	
 
 }
